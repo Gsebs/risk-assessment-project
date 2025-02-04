eea52c2bbd69e0fd7cbcf7a88ac161980e5a5d43
Gerald Sebastian <gerrysebs2018@gmail.com>
2025-02-04 17:01:52 -0600

    Updated .gitignore to exclude data and unnecessary files

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print("Hello, Risk Assessment Project")
-print("Test Push")
+from src.data_collection import get_fred_data
+
+# Get GDP data from FRED
+gdp_data = get_fred_data('GDP')
+print(gdp_data.head())
